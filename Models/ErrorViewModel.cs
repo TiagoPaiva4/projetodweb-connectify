9548613b72b7b8cd7784304f24d9b4ea65feb529
Tiago Paiva <paiva.tiagotp@gmail.com>
2025-04-03 20:49:02 +0100

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projetodweb_connectify.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
