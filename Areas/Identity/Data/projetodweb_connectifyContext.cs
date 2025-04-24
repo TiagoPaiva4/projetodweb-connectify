aadf587ecf7a5c9e444e0bea183757f534c292f2
Tiago Paiva <paiva.tiagotp@gmail.com>
2025-04-24 01:03:44 +0100

    Adição dos ficheiros da Identity que vão ser alterados

---
+++ Areas/Identity/Data/projetodweb_connectifyContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace projetodweb_connectify.Data;
+
+public class projetodweb_connectifyContext : IdentityDbContext<IdentityUser>
+{
+    public projetodweb_connectifyContext(DbContextOptions<projetodweb_connectifyContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
