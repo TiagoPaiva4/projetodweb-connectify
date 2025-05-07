2019f9645b3bf74f533c4cc3822f17381c86825c
bachilak <109044345+Mario-Bonacho@users.noreply.github.com>
2025-05-07 11:29:50 +0100

    alteracao no email settings

--- Services/Email/EmailSettings.cs
+++ Services/Email/EmailSettings.cs
@@ -2,7 +2,6 @@ namespace projetodweb_connectify.Services.Email;
 
 public class EmailSettings
 {
-    public string Smtp = "EmailSettings";
     public string SmtpServer { get; set; } = string.Empty;
     public int Port { get; set; }
     public string Username { get; set; } = string.Empty;
