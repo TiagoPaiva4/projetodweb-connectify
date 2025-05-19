97733923d25541f5487a18954194c4d8350fbfdb
Tiago Paiva <paiva.tiagotp@gmail.com>
2025-05-19 22:49:49 +0100

    criação de dados para serem usados em PRODUÇÃO

---
+++ Data/Migrations/20250519184405_UpdateSeed.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace projetodweb_connectify.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "a", "a3b119ca-fe9a-494f-8bd9-c5caaf1da86e", "admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "admin", 0, "7dbcba99-e2c9-4d11-9201-c5ea145390a6", "admin@mail.pt", true, false, null, "ADMIN@MAIL.PT", "ADMIN@MAIL.PT", "AQAAAAIAAYagAAAAEG5hJ1YL6QmZ7oVAlyNveu2uR/8ACXhBxT6yg7+8DHJxccaJ9ouU+mR76QbnmD84hA==", null, false, "bcc7416e-d54c-4d04-8264-98f427d04861", false, "admin@mail.pt" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "a", "admin" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "a", "admin" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "admin");
+        }
+    }
+}
